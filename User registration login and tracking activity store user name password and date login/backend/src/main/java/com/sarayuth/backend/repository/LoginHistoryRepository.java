270eeba47378044681a4ecb40b42649b27830ffc
@rayuthek430 <rayuthek430@gmail.com>
2025-05-11 16:08:55 +0900

    clear

--- User registration login and tracking activity store user name password and date login/backend/src/main/java/com/sarayuth/backend/repository/LoginHistoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sarayuth.backend.repository;
-
-import com.sarayuth.backend.model.LoginHistory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LoginHistoryRepository extends JpaRepository<LoginHistory, Long> {
-}
